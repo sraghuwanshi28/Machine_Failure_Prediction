798ef4bd764ac440adb549418af58ca11f1f62dc
sraghuwanshi28 <sraghuwanshi.ml@gmail.com>
2025-08-03 16:04:12 +0530

    Update repo information

--- model_building/prep.py
+++ model_building/prep.py
@@ -46,6 +46,6 @@ for file_path in files:
     api.upload_file(
         path_or_fileobj=file_path,
         path_in_repo=file_path.split("/")[-1],  # just the filename
-        repo_id="praneeth232/machine-failure-prediction",
+        repo_id="sraghuwanshi04/Machine-Failure-Prediction",
         repo_type="dataset",
     )
--- model_building/train.py
+++ model_building/train.py
@@ -89,7 +89,7 @@ print(classification_report(ytest, y_pred_test))
 joblib.dump(best_model, "best_machine_failure_model_v1.joblib")
 
 # Upload to Hugging Face
-repo_id = "praneeth232/machine_failure_model"
+repo_id = "sraghuwanshi04/Machine-Failure-Prediction"
 repo_type = "model"
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
